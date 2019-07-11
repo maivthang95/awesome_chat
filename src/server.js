2abdff71e51caad333c7b34b63a38a5d71ff6df6
maivthang95 <maivthang95@gmail.com>
2019-07-11 23:33:53 +0700

    [v.0.7]

--- src/server.js
+++ src/server.js
@@ -1,24 +1,20 @@
 import express from "express";
 import connectDB from "./config/connectDB";
 import contactModel from "./models/contact.model";
+import configViewEngine from "./config/viewEngine";
+// Init app
 let app = express() ;
 
 connectDB();
 
-app.get("/contact", async (req , res) => {
-  try {
-    let item = {
-      userId : "328193793", 
-      contactId : "a23781291"
-    }
-    
-  let contact = await contactModel.createNew(item) ;
-  res.send(contact)
- 
-  } catch (error) {
-    console.log(error);
-  }
+//Config view engine 
+configViewEngine(app) ;
+app.get("/",(req , res) => {
+  res.render("main/master")
 })
 
+app.get("/login-register",(req , res) => {
+  res.render("auth/loginRegister")
+})
 app.listen(process.env.APP_PORT , process.env.APP_HOST , () => console.log(`Server is running on port ${process.env.APP_PORT} , host : ${process.env.APP_HOST }`));
 
