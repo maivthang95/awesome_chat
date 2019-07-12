2c75d41e23e40bf04d628b69af65eb3f45ed840d
maivthang95 <maivthang95@gmail.com>
2019-07-12 21:21:36 +0700

    [v.0.12] send_email_confirm_account

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,11 +1,12 @@
 import userModel from "./../models/userModel";
 import bcrypt from "bcrypt" ;
 import uuidv4 from "uuid/v4";
-import {transErrors, transSuccess} from "./../../lang/vi";
+import {transErrors, transSuccess , transMail} from "./../../lang/vi";
+import sendMail from "./../config/mailer" ;
 
 let saltRound = 7 ;
 
-let register = (email, gender, password) => {
+let register = (email, gender, password , protocol , host) => {
   return new Promise( async (resolve , reject) =>{
     let userByEmail = await userModel.findEmail(email) ;
     if(userByEmail){
@@ -30,12 +31,34 @@ let register = (email, gender, password) => {
     };
 
     let user = await userModel.createNew(userItem) ;
+    //send email
+    let linkVerify = `${protocol}://${host}/verify/${user.local.verifyToken}`
+    sendMail(email , transMail.subject , transMail.template(linkVerify))
+    .then( success => {
+      resolve(transSuccess.userCreated(user.local.email));
+    })
+    .catch( async error => {
+      await userModel.removeById(user._id);
+      console.log(error) ;
+      reject(error)
+    })
     resolve(transSuccess.userCreated(user.local.email));
   })
   
 }
 
+let verifyAccount = (token) => {
+  return new Promise( async (resolve , reject) => {
+    let UserByToken = await userModel.findByToken(token) ;
+    if(!UserByToken){
+      return reject(transErrors.token_undifined);
+    }
+    await userModel.verify(token) ;
+    resolve(transSuccess.account_active); 
+  })
+}
 module.exports = {
-  register : register
+  register : register,
+  verifyAccount : verifyAccount
 }
 
