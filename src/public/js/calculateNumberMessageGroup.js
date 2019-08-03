2d30441ab697b4a6bc75d7b1ea09cab6def13dc8
maivthang95 <maivthang95@gmail.com>
2019-08-03 07:19:37 +0700

    [v.0.42] chat_text_message_part3

---
+++ src/public/js/calculateNumberMessageGroup.js
@@ -0,0 +1,7 @@
+function increaseNumberMessageGroup(divId){
+    let currentValue = +$(`.right[data-chat = ${divId}] `).find(".show-number-messages").text();
+    currentValue++;
+    
+        $(`.right[data-chat = ${divId}] `).find(".show-number-messages").html(currentValue);
+    
+}
--- src/public/js/textAndEmojiChat.js
+++ src/public/js/textAndEmojiChat.js
@@ -1,8 +1,10 @@
 function textAndEmojiChat(divId){
   $(".emojionearea").off("keyup").on("keyup" , function(element){
+    let currentEmojioneArea = $(this);
     if(element.which == 13){
       let targetId = $(`#write-chat-${divId}`).data("chat");
       let messageVal = $(`#write-chat-${divId}`).val();
+
       if(!targetId.length || !messageVal.length){
         return false ;
       }
@@ -15,7 +17,34 @@ function textAndEmojiChat(divId){
         dataTextEmojiForSend.isChatGroup = true ;
       }
       $.post("/message/add-new-text-emoji" , dataTextEmojiForSend , function(data){
-        console.log(data);
+        
+        let myMessage = $(`<div class="bubble me" data-mess-id="${data.message_id}"></div>`);
+        if(dataTextEmojiForSend.isChatGroup){
+          myMessage.html(`<img src="images/users/${data.sender.avatar}" class="avatar-small" title="${ data.sender.name}">`)
+          myMessage.text(data.message.text);
+          increaseNumberMessageGroup(divId)
+        }else{
+          myMessage.text(data.message.text);
+        }
+        let convertEmojiMessage = emojione.toImage(myMessage.html());
+        myMessage.html(convertEmojiMessage) ;
+        console.log(convertEmojiMessage);
+        $(`.right .chat[data-chat = ${divId}] `).append(myMessage);
+        nineScrollRight(divId);
+
+
+        $(`#write-chat-${divId}`).val("");
+        currentEmojioneArea.find(".emojionearea-editor").text("");
+        $(`.person[data-chat=${divId}]`).find(".preview").html(emojione.toImage(data.message.text));
+        $(`.person[data-chat=${divId}]`).find("span.time").html(moment(data.message.createdAt).locale("vi").startOf("seconds").fromNow());
+
+        $(`.person[data-chat=${divId}]`).on("click.moveConversationToTop" , function(){
+          let dataToMove = $(this).parent();
+          $(this).closest("ul").prepend(dataToMove);
+          $(this).off("click.moveConversationToTop");
+        })
+
+        $(`.person[data-chat=${divId}]`).click();
       }).fail(function( response ){
         alertify.notify(response.responseText , "error" , 5);
       })
